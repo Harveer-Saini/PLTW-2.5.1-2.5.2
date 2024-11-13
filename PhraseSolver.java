2b5af6d92cda0fe7b748d7383c4e822eb0f0a4d7
Harveer-Saini <saini8304@mydusd.org>
2024-11-13 14:41:08 -0800

    Create PhraseSolver.java

---
+++ PhraseSolver.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+  
+public class PhraseSolver
+{
+  /* your code here - attributes */
+  Player player1;
+  Player player2;
+  Board board;
+  boolean solved;
+  /* your code here - constructor(s) */ 
+  public PhraseSolver() {
+    player1 = null;
+    player2 = null;
+    board = null;
+    solved = false;
+  }
+
+  public void play()
+  {
+    boolean solved = false;
+    int currentPlayer = 1;
+    Scanner input = new Scanner(System.in);
+    
+    boolean correct = true;
+    while (!solved) 
+    {
+      
+      solved = true; 
+    } 
+   
+  }
+  
+}
